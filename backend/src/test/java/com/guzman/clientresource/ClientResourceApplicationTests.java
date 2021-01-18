1104a1b7f15ca4dde2fbfbdf108d883ef91fb5e9
rqguzman <rafael.guzman@outlook.com>
2021-01-18 08:09:34 -0300

    :tada: Project created

---
+++ backend/src/test/java/com/guzman/clientresource/ClientResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzman.clientresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
