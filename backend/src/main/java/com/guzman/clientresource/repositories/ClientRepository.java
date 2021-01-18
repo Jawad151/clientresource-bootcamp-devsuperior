aca032ea1faa826a0b1a3593194c6b51d4972bcf
rqguzman <rafael.guzman@outlook.com>
2021-01-18 14:39:12 -0300

    :sparkles:feat(ClientResource):Add data access layer

---
+++ backend/src/main/java/com/guzman/clientresource/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.guzman.clientresource.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guzman.clientresource.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+}
